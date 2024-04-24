69ae31d0a7acf4971e9026e2621480ad4d1cb638
galeNight <jannickmorup@gmail.com>
2024-04-24 10:36:31 +0200

    added comment

--- src/app/Components/Misc/error/error.component.ts
+++ src/app/Components/Misc/error/error.component.ts
@@ -1,12 +1,13 @@
+// Importing the Component decorator from the Angular core module
 import { Component } from '@angular/core';
-
+// Decorating the ErrorComponent class with metadata
 @Component({
-  selector: 'app-error',
-  standalone: true,
-  imports: [],
-  templateUrl: './error.component.html',
-  styleUrl: './error.component.css'
+  selector: 'app-error', // Selector used to identify this component in HTML templates
+  standalone: true, // Custom property (standalone) for internal purposes or custom usage
+  imports: [], // An empty array of imports (typically used for importing Angular modules or other components/services)
+  templateUrl: './error.component.html', // Path to the HTML template file associated with this component
+  styleUrl: './error.component.css' // Path to the CSS file associated with this component
 })
-export class ErrorComponent {
+export class ErrorComponent { // Defining the ErrorComponent class
 
 }
